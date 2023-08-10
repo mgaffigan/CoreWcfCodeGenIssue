ec2811a75fb9ce5e257a1522a4fe86af56205c7b
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2023-08-10 10:41:58 -0500

    Working

--- Program.cs
+++ Program.cs
@@ -4,25 +4,28 @@ using System.ServiceModel;
 // Provide main
 Console.WriteLine();
 
-[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
-sealed class ExampleAttribute : Attribute
+namespace Demo
 {
-    public ExampleAttribute(params string[] args)
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    sealed class ExampleAttribute : Attribute
     {
+        public ExampleAttribute(params string[] args)
+        {
+        }
     }
-}
 
-[ServiceContract]
-public interface IService
-{
-    [OperationContract]
-    void Operation();
-}
-
-public partial class Service : IService
-{
-    //[Example("A")]
-    public void Operation([FromServices] HttpContext hc)
+    [ServiceContract]
+    public interface IService
     {
+        [OperationContract]
+        void Operation();
+    }
+
+    public partial class Service : IService
+    {
+        //[Example("A")]
+        public void Operation([FromServices] HttpContext hc)
+        {
+        }
     }
 }
