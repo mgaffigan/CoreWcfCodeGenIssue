9232411c7d34fa8d573ca6c3205cd8fa56848ac8
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2023-08-10 10:32:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ServiceModel;
+
+// Provide main
+Console.WriteLine();
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+sealed class ExampleAttribute : Attribute
+{
+    public ExampleAttribute(params string[] args)
+    {
+    }
+}
+
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    void Operation();
+}
+
+public partial class Service : IService
+{
+    //[Example("A")]
+    public void Operation([FromServices] HttpContext hc)
+    {
+    }
+}
